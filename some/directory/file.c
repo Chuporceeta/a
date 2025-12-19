4c5e1b6df887ce542892a094c12ac55b9968c7b0
Xabier Sienra <47067406+Chuporceeta@users.noreply.github.com>
2025-12-19 12:57:23 -0500

    Update file.c

--- some/directory/file.c
+++ some/directory/file.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-
-  
-  // This is a line from the original file that was not modified. It is included
-  // as context to make it easier to read the patch file.
-  // This is a line in the original file that was removed while editing.
-  // This is another line that was not modified.
-
-  
-  return 0;
+    // This is a line from the original file that was not modified. It is included
+    // as context to make it easier to read the patch file.
+    // This is a line in the original file that was removed while editing.
+    // This is another line that was not modified.
+    // This line uses spaces for indentation.
+	// This line uses tabs for indentation.
+	// filler
+	// filler
+	// filler
+	// filler
+    return 0;
 }
