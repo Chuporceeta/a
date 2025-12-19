b1f5a3095a38ed6fe366b7c1ca591fc9d1422d2d
Xabier Sienra <47067406+Chuporceeta@users.noreply.github.com>
2025-12-19 13:12:09 -0500

    Update file.c

--- some/directory/file.c
+++ some/directory/file.c
@@ -7,9 +7,9 @@ int main() {
     // as context to make it easier to read the patch file.
     //
     // This is a line in the original file that was removed while editing.
-    // This is another line that was not modified.
-    // Every line in the patch file starts with a space (context), - (removal), or + (addition).
-	// The full line then follows, including leading whitespace (notice this line has a tab instead of spaces)
+    // 
+	// Every line in the patch file starts with a space (context), - (removal), or + (addition).
+	// The full line then follows, including leading whitespace (notice tabs instead of spaces here)
 	//
 	//
     return 0;
