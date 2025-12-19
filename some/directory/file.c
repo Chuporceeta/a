d0d44d3721acf7ba47359f5b5e5c150cfdd7c13c
Xabier Sienra <47067406+Chuporceeta@users.noreply.github.com>
2025-12-19 12:46:25 -0500

    Create file.c

---
+++ some/directory/file.c
@@ -0,0 +1,8 @@
+int main() {
+  // This is a line from the original file that was not modified. It is included
+  // as context to make it easier to read the patch file.
+  // This is a line in the original file that was modified.
+  // This is a line in the original file that was removed.
+  // This is another line that was not modified.
+  return 0;
+}
