72a2ea2d1c76617ecf57ce535fbf8276f40c4d61
Xabier Sienra <47067406+Chuporceeta@users.noreply.github.com>
2025-12-19 13:07:13 -0500

    Update file.c

--- some/directory/file.c
+++ some/directory/file.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    // This is a line from the original file that was not modified. It is included
+    // These lines are from the original file and were not modified. They is included
     // as context to make it easier to read the patch file.
+    // Every line in the patch file starts with a space (context), - (removal), or + (addition).
+	// The full line then follows, including leading whitespace (notice this line has a tab instead of spaces)
+	//
     // This is a line in the original file that was removed while editing.
     // This is another line that was not modified.
-    // This line uses spaces for indentation.
-	// This line uses tabs for indentation.
-	// filler
-	// filler
-	// filler
-	// filler
+    // filler
+    // filler
+    // filler
+    // filler
     return 0;
 }
